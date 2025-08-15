871da63230ce446ccd384d20baadfc5fde8a9fbc
Tirth-22 <tirthmakadia2006@gmail.com>
2025-08-15 08:47:47 +0530

    Added player file

---
+++ src/model/Player.java
@@ -0,0 +1,26 @@
+package model;
+
+public class Player {
+    private String name;
+    private String position;
+
+    public Player(String name, String position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setPosition(String position){
+        this.position = position;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+    public String getPosition(){
+        return this.position;
+    }
+
+}
